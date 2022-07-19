1ae40599b6263017f32f72d85c15f317ad51d942
Mario Guerrieri <mario@guerrieri.codes>
2022-07-19 10:04:56 -0700

    Break lines

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 let package = Package(
     name: "NavigationCompat",
     platforms: [
-        .iOS(.v13), .watchOS(.v7), .macOS(.v11), .tvOS(.v13),
+        .iOS(.v13),
+        .watchOS(.v7),
+        .macOS(.v11),
+        .tvOS(.v13),
     ],
     products: [
         .library(
